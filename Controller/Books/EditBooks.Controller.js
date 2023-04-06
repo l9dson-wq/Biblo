078107e7a5959417d5760e81c0549a72119b5e78
Juancdelarosa <juancdelarosa2710@gmail.com>
2023-04-06 15:15:50 -0400

    Trabajando en el Edit de Libros

---
+++ Controller/Books/EditBooks.Controller.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
+
+const EditBooks = async (req, res) =>{
+
+    let bookID = req.body.bookID;
+    
+    try {
+
+
+    } catch(error){
+        console.log(error)
+    }
+}
+
