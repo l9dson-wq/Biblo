1c50009c727c99b28ea43809404b3b649c2172f6
Andelson Lopez <69158247+l9dson-wq@users.noreply.github.com>
2023-04-10 21:16:44 -0400

    Some important changes to shopping car

--- Controller/ViewsRender/RenderCesta.Controller.js
+++ Controller/ViewsRender/RenderCesta.Controller.js
@@ -4,13 +4,29 @@ const session = require("express-session");
 const getAllShoppingCarModel = require("../../Model/Books/ShoppingCar.Model");
 
 const RenderCesta = async (req, res) => {
-    let userID = req.session.userId;
+  let userID = req.session.userId;
 
-    let totalShoppingCar = await getAllShoppingCarModel.getAllShoppingCar(userID);
-    let totalShoppingCarNumber = Number(totalShoppingCar.length);
-    console.log(totalShoppingCarNumber);  
+  let totalShoppingCar = await getAllShoppingCarModel.getAllShoppingCar(userID);
+  let totalShoppingCarNumber = Number(totalShoppingCar.length);
+  console.log(totalShoppingCar);
 
-    res.render("Cesta", { session: req.session, totalNoti: totalShoppingCarNumber, });
+  let totalPrice = 0;
+  let numeroLimitado;
+
+  for( let i = 0; i < totalShoppingCar.length; i++ ){
+    totalPrice += totalShoppingCar[i].totalPrice;
+  }
+
+ totalPrice = parseFloat(totalPrice.toFixed(2));
+
+  console.log(totalPrice);
+
+  res.render("Cesta", {
+    session: req.session,
+    totalNoti: totalShoppingCarNumber,
+    allShoppingCar: totalShoppingCar,
+    subtotal: totalPrice,
+  });
 };
 
-module.exports = { RenderCesta, };
+module.exports = { RenderCesta };
